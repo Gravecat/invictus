c6b4bee69d867ca2aaa6696013eedb36e809a178
Raine Simmons <gc@gravecat.com>
2023-05-25 16:23:21 +0100

    Added timing definitions for in-game events.

---
+++ src/world/readme.md
@@ -0,0 +1,5 @@
+# src/world
+
+The world source folder contains code that affects the game world as a whole, but doesn't belong in any other particular sub-folder.
+
+* **timing.hpp** - `constexpr` values for the time it takes to perform various actions in-game, or other fixed timing values.
---
+++ src/world/timing.hpp
@@ -0,0 +1,22 @@
+// core/timing.hpp -- All definitions for timing in the game (i.e. how long actions take to perform).
+// Copyright © 2023 Raine "Gravecat" Simmons. Licensed under the GNU Affero General Public License v3 or any later version.
+
+#ifndef CORE_TIMING_HPP_
+#define CORE_TIMING_HPP_
+
+namespace invictus
+{
+
+class Timing
+{
+public:
+    static constexpr float  TICK_SPEED =            0.1f;   // How often does the heartbeat tick? (Don't change this. I mean it.)
+    static constexpr float  TIME_BASE_MOVEMENT =    1.0f;   // The basic movement speed for moving a single tile, unmodified by armour or other attributes.
+                                                            // Probably best to not adjust this one, it might make things weird.
+    static constexpr float  TIME_CLOSE_DOOR =       0.5f;   // The time it takes to close a door.
+    static constexpr float  TIME_DO_NOTHING =       1.0f;   // The time spent when pressing the 'do nothing' key.
+    static constexpr float  TIME_OPEN_DOOR =        0.5f;   // The time it takes to open a door.
+};
+
+}       // namespace invictus
+#endif  // CORE_TIMING_HPP_
